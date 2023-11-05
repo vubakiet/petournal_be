8cb027716f64b4d97ee0751e762da04366d7c2ec
VuKiet <81238426+vubakiet@users.noreply.github.com>
2023-11-05 21:52:06 +0700

    .
    .

--- src/models/base/User.js
+++ src/models/base/User.js
@@ -2,32 +2,32 @@ import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
         _id: mongoose.Schema.Types.ObjectId,
-        firstName: { type: String, required: true },
+        firstName: { type: String },
         lastName: { type: String, required: true },
         email: {
             type: String,
             required: true,
             trim: true,
             unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"],
         },
         password: { type: String, required: true },
-        role: { type: String, required: true, trim: true },
+        role: { type: String, trim: true },
         avatar: { type: String },
-        bio: {type: String},
+        bio: { type: String },
         birthday: { type: Date },
-        gender: {type: Boolean},
-        phone: { type: String, required: true, trim: true, unique: true },
-        address: { type: String, required: true, trim: true },
+        gender: { type: Boolean },
+        phone: { type: String, trim: true },
+        address: { type: String, trim: true },
     },
     {
         timestamps: true,
         versionKey: false,
-        collection: 'User'
-    },
+        collection: "User",
+    }
 );
 
-export default mongoose.model('User', userSchema);
-
+export default mongoose.model("User", userSchema);
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { DB_LINK, PORT } from "./config/index.js";
 import routes from "./routes/index.js";
 import { Server } from "socket.io";
 import Gateway from "./socket/gateway.js";
+import cors from "cors"
 
 const app = express();
 const server = http.createServer(app);
@@ -25,6 +26,21 @@ mongoose
     });
 
 const startServrver = () => {
+    var allowedDomains = ["http://localhost:3000"];
+    app.use(cors({
+        origin: function(origin, callback) {
+            // bypass the requests with no origin (like curl requests, mobile apps, etc )
+            if (!origin) return callback(null, true);
+
+            if (allowedDomains.indexOf(origin) === -1) {
+                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        credentials: true,
+    }));
+
     app.use(cookieParser());
     app.use(express.json({ limit: "50mb" }));
     app.use(express.urlencoded({ extended: true, limit: "50mb" }));
